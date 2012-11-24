e6e55e9a24dccf644887ef82bd6347609e03f5e3
Ed Ellson <ed@mintdigital.com>
2012-11-24 16:45:01 +0000

    Removes old commented out code

--- filter.rb
+++ filter.rb
@@ -9,19 +9,17 @@ WORDS = IO.read('weather_words.txt')
 TweetStream::Client.new.locations(-180,-90,180,90) do |status|
   # Ignore replies
   if (status.text !~ /^@\w+/ && status.text =~ /\s+(#{WORDS})\s+/)
-    # if status.text ~ /.+(weather|sunny|rain|storm).+/i
-      STORE.push(
-            'id' => status[:id],
-            'text' => status.text,
-            'username' => status.user.screen_name,
-            'userid' => status.user[:id],
-            'name' => status.user.name,
-            'profile_image_url' => status.user.profile_image_url,
-            'created_at' => status.created_at,
-            'received_at' => Time.new.to_i,
-            'place_name' => status.place.name,
-            'coordinates' => status.place.bounding_box.coordinates
-      )
-    # end
+    STORE.push(
+          'id' => status[:id],
+          'text' => status.text,
+          'username' => status.user.screen_name,
+          'userid' => status.user[:id],
+          'name' => status.user.name,
+          'profile_image_url' => status.user.profile_image_url,
+          'created_at' => status.created_at,
+          'received_at' => Time.new.to_i,
+          'place_name' => status.place.name,
+          'coordinates' => status.place.bounding_box.coordinates
+    )
   end
 end
