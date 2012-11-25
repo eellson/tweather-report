7ce369121bb91c7502161f7815006ed60741214a
Ed Ellson <ed@mintdigital.com>
2012-11-25 16:00:47 +0000

    Adds inital sentiment gathering app

---
+++ sentiment.rb
@@ -0,0 +1,13 @@
+require 'json'
+require 'net/http'
+
+uri = URI('http://www.sentiment140.com/api/bulkClassifyJson')
+params = {'data' => [{'text' => 'I love Titanic.'}]}.to_json
+req = Net::HTTP::Post.new(uri.path, initheader = {'Content-Type' =>'application/json'})
+req.body = params
+
+res = Net::HTTP.start(uri.hostname, uri.port) do |http|
+  http.request(req)
+end
+
+puts res.body
